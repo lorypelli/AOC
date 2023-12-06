7f18cfbeb4012fa74f1ebd804cd67f185abf6e36
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-12-06 22:03:12 +0100

    Finished day 2 of 2022

--- 2022/2/main.go
+++ 2022/2/main.go
@@ -97,7 +97,100 @@ func score(arr []string) int {
 	return s
 }
 
+func secondScore(arr []string) int {
+	s := 0
+	choice := 0
+	score := 0
+	for i := 0; i < len(arr); i++ {
+		switch string(arr[i][0]) {
+		case "A":
+			{
+				switch string(arr[i][2]) {
+				case "X":
+					{
+						choice = 3
+						score = choice + 0
+						s += score
+						break
+					}
+				case "Y":
+					{
+						choice = 1
+						score = choice + 3
+						s += score
+						break
+					}
+				case "Z":
+					{
+						choice = 2
+						score = choice + 6
+						s += score
+						break
+					}
+				}
+				break
+			}
+		case "B":
+			{
+				switch string(arr[i][2]) {
+				case "X":
+					{
+						choice = 1
+						score = choice + 0
+						s += score
+						break
+					}
+				case "Y":
+					{
+						choice = 2
+						score = choice + 3
+						s += score
+						break
+					}
+				case "Z":
+					{
+						choice = 3
+						score = choice + 6
+						s += score
+						break
+					}
+				}
+				break
+			}
+		case "C":
+			{
+				switch string(arr[i][2]) {
+				case "X":
+					{
+						choice = 2
+						score = choice + 0
+						s += score
+						break
+					}
+				case "Y":
+					{
+						choice = 3
+						score = choice + 3
+						s += score
+						break
+					}
+				case "Z":
+					{
+						choice = 1
+						score = choice + 6
+						s += score
+						break
+					}
+				}
+				break
+			}
+		}
+	}
+	return s
+}
+
 func main() {
 	arr := globals.SplitFile("input.txt")
-	fmt.Printf("The total score is %d", score(arr))
+	fmt.Printf("The total score is %d\n", score(arr))
+	fmt.Printf("The total score with the new strategy is %d", secondScore(arr))
 }
