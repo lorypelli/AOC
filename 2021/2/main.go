bc79ad46c256ef5c0ba02bf103d85d4996c5a863
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-12-08 20:56:13 +0100

    Finished day 2 of 2021

--- 2021/2/main.go
+++ 2021/2/main.go
@@ -34,7 +34,37 @@ func steps(arr []string) int {
 	return h * d
 }
 
+func newSteps(arr []string) int {
+	h := 0
+	d := 0
+	a := 0
+	for i := 0; i < len(arr); i++ {
+		move := strings.Split(arr[i], " ")[0]
+		step, _ := strconv.Atoi(strings.Split(arr[i], " ")[1])
+		switch move {
+		case "forward":
+			{
+				h += step
+				d += a * step
+				break
+			}
+		case "down":
+			{
+				a += step
+				break
+			}
+		case "up":
+			{
+				a -= step
+				break
+			}
+		}
+	}
+	return h * d
+}
+
 func main() {
 	arr := globals.SplitFile("input.txt")
-	fmt.Printf("Your final horizontal position and your final depth position multiplied together give %d", steps(arr))
+	fmt.Printf("Your final horizontal position and your final depth position multiplied together give %d\n", steps(arr))
+	fmt.Printf("Your final horizontal position and your final depth position multiplied together based on new counting process give %d", newSteps(arr))
 }
